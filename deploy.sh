30ea2b2e930cdf2b8333d0cf60962ef4a6044783
Stephen Cagle <stephen@read-line.com>
2019-06-26 20:04:51 -0700

    AUTO COMMIT

--- deploy.sh
+++ deploy.sh
@@ -2,15 +2,15 @@
 
 starting_pwd=pwd
 
-porcelain=$(git status --porcelain)
+# porcelain=$(git status --porcelain)
 
-if [ -n "$porcelain" ]
-then
-    echo "PLEASE COMMIT YOUR CHANGE FIRST!!!"
-    exit 1
-fi
+# if [ -n "$porcelain" ]
+# then
+#     echo "PLEASE COMMIT YOUR CHANGE FIRST!!!"
+#     exit 1
+# fi
 
-branch=$(git branch | grep -e "^*" | cut -d' ' -f 2)
+# branch=$(git branch | grep -e "^*" | cut -d' ' -f 2)
 lein run
 cp -r public/blog/* ~/firemore-docs
 cp ~/firemore-docs
